dcb6028f5c23522a09686eca7a4e23c8dc8ff3a2
jess <jess@jesss-MacBook-Pro.local>
2024-06-29 18:48:41 +0800

    add pinggy error message

--- src/Outlet/OutletList.js
+++ src/Outlet/OutletList.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import OutletInfo from './OutletInfo'; 
+import OutletInfo from './OutletInfo';
 import './OutletList.css';
 
 
 const OutletList = ({ outlets, handleMarkerMouseOver, handleMarkerMouseOut, hoveredOutletId }) => {
     return (
         <div className="outlet-list">
-            {outlets.map((outlet) => (
-                <div key={outlet.id} 
-                className={`outlet-item ${hoveredOutletId === outlet.id ? 'highlighted' : ''}`}
-                onMouseEnter={() => handleMarkerMouseOver(outlet.id)}
-                onMouseLeave={handleMarkerMouseOut}>
-                <OutletInfo outlet={outlet} />
+            {outlets.length === 0 ? (
+                <div className="empty-outlet-message">
+                    <h6 className="outlet-name">{"No outlets available."}</h6>
+                    The local server might be down (possible Pinggy issue).
+                    Please run the backend locally and update <strong>API_BASE_URL</strong> in <em>constants.js</em> to <code>localhost</code>.
                 </div>
-            ))}
+            ) : (
+                outlets.map((outlet) => (
+                    <div key={outlet.id}
+                        className={`outlet-item ${hoveredOutletId === outlet.id ? 'highlighted' : ''}`}
+                        onMouseEnter={() => handleMarkerMouseOver(outlet.id)}
+                        onMouseLeave={handleMarkerMouseOut}>
+                        <OutletInfo outlet={outlet} />
+                    </div>
+                ))
+            )}
         </div>
     );
 };
